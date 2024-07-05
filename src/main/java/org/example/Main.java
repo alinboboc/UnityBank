ae30e6893149e7f3e860a46014b5fd911a49f370
Alin Boboc <boboc.alin.ro@gmail.com>
2024-07-05 08:44:20 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,15 +9,3 @@ public class Main {
         LandingPage app = new LandingPage();
     }
 }
-
-/*
-TODO LIST:
-1. need to add uppercase validation for first name and last name before pushing in SQL database
-2. need to add verification for credit request, if a credit is active and the monthly pay is 40% the credit cannot be received
-3. need to check if session user is still required after the code refactor
-4. need to add the functionality to modify the account details
-5. need to recheck all the variables from classes because some of them doesn't have any meaning
-6. need to test if i can use only one result set to update and read data from SQL,
-7. register nu merge
-8. NEED TO SET USER NAME LOCALLY TO UPDATE THE WELCOME MESSAGE.
-*/
